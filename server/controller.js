1831dc41d097588b2bc84ab500e1d4456fac978a
Akshay Veera <akshayveera01@gmail.com>
2025-01-20 09:36:00 +0530

    add proxy server

--- server/controller.js
+++ server/controller.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const pool = require('./db');
 
 const getComponent2 = (req, res) => {
@@ -31,4 +32,79 @@ const sendDataFromDB = (query, res) => {
     })
 }
 
-module.exports = { getComponent2, getComponent4, getComponent6 };
+
+const headers = new Headers();
+headers.set('Authorization', 'Basic ' + btoa(`${process.env.AUTH_USERNAME}:${process.env.AUTH_PASSWORD}`));
+
+const getComponent1 = async (req, res) => {
+    const URL = "http://3.111.196.92:8020/api/v1/sample_assignment_api_1/";
+
+    try {
+      const response = await fetch(URL, {headers: headers});
+      const data =  await response.json();
+      res.status(200).json(data);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+
+}
+
+const getComponent3 = async (req, res) => {
+    const URL = "http://3.111.196.92:8020/api/v1/sample_assignment_api_3/";
+
+    try {
+      const response = await fetch(URL, {headers: headers});
+      const data =  await response.json();
+      res.status(200).json(data);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+
+}
+
+const getComponent5 = async (req, res) => {
+    const URL = "http://3.111.196.92:8020/api/v1/sample_assignment_api_5/";
+
+    try {
+      const response = await fetch(URL, {headers: headers});
+      const data =  await response.json();
+      res.status(200).json(data);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+
+}
+
+const login = async (req, res) => {
+    const URL = "http://3.111.196.92:8020/api/v1/login/";
+
+    const bodyData = {
+        "username": req.body.username,
+        "email": "user@gmail.com",
+        "password": req.body.password,
+        "phone_number": "1234567890",
+        "input_code": 0
+    }
+
+    const response = await fetch(URL, {
+        method: 'POST', 
+        headers: {
+        "Content-Type": "application/json",
+        },
+        body: JSON.stringify(bodyData)
+    })
+
+    try {
+        const data = await response.json();
+    
+        res.status(200).json(data);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({err : "Internal Server Error"});
+    }
+}
+
+module.exports = { getComponent2, getComponent4, getComponent6, getComponent1, getComponent3, getComponent5, login };
--- server/routes.js
+++ server/routes.js
@@ -1,8 +1,16 @@
 const router = require('express').Router();
-const { getComponent2, getComponent4, getComponent6 } = require('./controller');
+const { getComponent2, getComponent4, getComponent6, getComponent1, getComponent3, getComponent5, login } = require('./controller');
 
 router.get('/component2', getComponent2);
 router.get('/component4', getComponent4);
 router.get('/component6', getComponent6);
+router.get('/component1', getComponent1);
+router.get('/component3', getComponent3);
+router.get('/component5', getComponent5);
+router.post('/login', login );
+
+router.use((req, res, next) => {
+    res.status(404).send('Route not found');
+});
 
 module.exports = router;
